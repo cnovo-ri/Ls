732926da2b6d78ab16c9104437d7ac6a2474bf4f
Carlos NOVO RIBEIRO <cnovo-ri@student.42.fr>
2017-10-05 22:53:19 +0200

    args

--- refactoring.c
+++ refactoring.c
@@ -6,7 +6,7 @@
 /*   By: cnovo-ri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 21:49:31 by cnovo-ri          #+#    #+#             */
-/*   Updated: 2017/09/19 20:55:14 by cnovo-ri         ###   ########.fr       */
+/*   Updated: 2017/10/05 22:52:55 by cnovo-ri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,44 @@ static char		*get_path(int argc, char *argv)
 {
 	char	*path;
 
-	if (!(path = (char *)malloc(sizeof(char) * 255)))
-		return (NULL);
-	if (argc == 1 && ft_strcmp(argv, "./ls/") > 0)
+	path = NULL;
+	if (ft_strcmp(argv, "./ls/") > 0)
 	{
+		ft_putendl(YELLOW"ERROR");
 		if (argv[ft_strlen(argv)] != '/')
-			path = ft_strjoin(argv, "/");
+		{
+			path = ft_strdup(ft_strjoin(argv, "/"));
+			return (path);
+		}
 		else
-			path = argv;
+		{
+			path = ft_strdup(argv);
+			return (path);
+		}
 	}
-	else
+	if (argc == 1)
 		path = "./";
 	return (path);
+}
+
+static char		**stock_args(int argc, char **argv)
+{
+	char	**tmp;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!(tmp = (char **)malloc(sizeof(char *) * argc)))
+		return (NULL);
+	while (i < argc)
+	{
+		if (argv[i][0] != '-')
+			tmp[j++] = argv[i];
+		i++;
+	}
+	tmp[j] = NULL;
+	return (tmp);
 }
 
 int			main(int argc, char **argv)
@@ -35,16 +61,25 @@ int			main(int argc, char **argv)
 	t_opts		*opts;
 	char		**tab;
 	char		*path;
+	char		**args;
 	int			i;
 	int			j;
 
 	j = 0;
 	opts = parsing(argc, argv);
-//	printf(YELLOW"argc :%d\n", argc);
-	while (argv[j])
+	args = stock_args(argc, argv);
+	printf(YELLOW"argc :%d\n"NORMAL, argc);
+	while (args[j])
 	{
+		printf(YELLOW"argv[%d] : %s\n", j, args[j]);
+		j++;
+	}
+//	while (argv[j])
+//	{
+//		printf(GREEN"\n\nargv :%s\n\n"NORMAL, argv[j]);
 		i = 0;
 		path = get_path(argc, argv[j]);
+//		printf(GREEN"\npath :%s\n\n"NORMAL, path);
 		tab = stock_directory(path);
 		if (opts->recursive == TRUE)
 			tab = press_r(tab, path);
@@ -73,7 +108,7 @@ int			main(int argc, char **argv)
 				ft_putendl(tab[i]);
 			i++;
 		}
-		j++;
-	}
+//		j++;
+//	}
 	return (0);
 }
